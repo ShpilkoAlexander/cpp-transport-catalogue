e2898d3a665ee57f6b30608d52fef988817ea367
ShpilkoAlexander <shpilko.alexander@gmail.com>
2022-08-13 12:18:21 +0300

    Sprint_9

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,21 @@
-// место для вашего кода
+#include <iostream>
+
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    TransportCatalogue transport_catalogue;
+
+    InputReader reader(transport_catalogue);
+    reader.ProcessingRequest(std::cin);
+
+    StatReader stat_reader(transport_catalogue, std::cin, std::cout);
+    stat_reader.ProcessingStat();
+
+}
